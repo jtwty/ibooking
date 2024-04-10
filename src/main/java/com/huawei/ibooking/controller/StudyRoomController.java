8f21215ccb93da8e8601b7502038a60b6b01294c
jcwang <1403937048@qq.com>
2024-04-10 18:55:46 +0800

    update StudyRoomController

--- src/main/java/com/huawei/ibooking/controller/StudyRoomController.java
+++ src/main/java/com/huawei/ibooking/controller/StudyRoomController.java
@@ -1,12 +1,25 @@
 package com.huawei.ibooking.controller;
 
+import com.huawei.ibooking.business.StudyroomBusiness;
+import com.huawei.ibooking.model.StudyRoomDO;
+import jakarta.annotation.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class StudyRoomController {
+    @Resource
+    private StudyroomBusiness studyroomBusiness;
+
     @GetMapping(value = "/studyroom")
-    public String studyroom() {
-        return "studyroom";
+    public ResponseEntity<List<StudyRoomDO>> Lists() {
+
+        List<StudyRoomDO> studyrooms=studyroomBusiness.getStudyRooms();
+        return new ResponseEntity<>(studyrooms, HttpStatus.OK);
     }
 }
